3cc18c9d266b70f1ed09b7485aadeb7966d72023
fitaa8228 <fitaa8228@HRH-IC0027347.wrdsb.ca>
2016-11-08 10:11:06 -0500

    Completed 1 & 2

---
+++ src/A6Q2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fitaa8228
+ */
+public class A6Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Call method
+        examGrade(75);
+    }
+    //Make method
+
+    public static void examGrade(double grade) {
+        //Check what the grade is and output a letter accordingly
+        if (grade < 50) {
+            System.out.println("F");
+        } else if (grade >= 50
+                && grade <= 59) {
+            System.out.println("D");
+        } else if (grade >= 60
+                && grade <= 69) {
+            System.out.println("C");
+        } else if (grade >= 70
+                && grade <= 79) {
+            System.out.println("B");
+        } else if (grade >= 80
+                && grade <= 100) {
+            System.out.println("A");
+        } else if (grade > 100) {
+            System.out.println("Cheater");
+        }
+    }
+}
---
+++ src/A6Q9.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fitaa8228
+ */
+public class A6Q9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
