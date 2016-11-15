5d8501bf73012d16209a60144fc3957b6c5ea13d
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2016-11-15 10:02:04 -0500

    a6q2 start

--- src/a6q2.java
+++ src/a6q2.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +15,36 @@ public class a6q2 {
     /**
      * @param args the command line arguments
      */
+    
+        
+    public static double examGrade(double examgrade){
+        double answer = Math.pow(examgrade,2);
+        return answer;
+        
+    }
+       
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        //create input for the user
+        Scanner input= new Scanner(System.in);
+        System.out.println("Please type in exam grade in %");
+        //let user answer
+        int grade = input.nextInt();
+        //create conditions
+        if(grade<50){
+            System.out.println("F");
+        }else if(grade<59){
+            System.out.println("D");
+        }else if(grade<69){
+            System.out.println("C");
+        }else if(grade<79){
+            System.out.println("B");
+        }else
+            System.out.println("A");
+        
+        
+        
+        
     }
 }
