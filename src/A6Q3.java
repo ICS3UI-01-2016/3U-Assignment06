02f5c9d96629fa276afc29bfdb3e8a171abe60c0
manok5757 <manok5757@HRH-IC0027323.wrdsb.ca>
2016-11-16 10:15:51 -0500

    A5 complete

---
+++ src/A6Q3.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author manok5757
+ */
+public class A6Q3 {
+
+    // create a method to list all the factors of a number
+    public static int factors(int num){
+        int num1= 0;
+        for(int numbers = 0; numbers < num; numbers = numbers+1){
+        if(int factor % 2 == 0 ){
+            
+        }
+        
+        
+    }
+        
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scenner to collect input
+        Scanner input = new Scanner(System.in);
+    }
+}
