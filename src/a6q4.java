8d9de3dbf071eed3ad92ae6c9deffdcdc2728d8c
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2016-11-16 10:16:13 -0500

    a6q3 start

--- src/a6q4.java
+++ src/a6q4.java
@@ -12,7 +12,21 @@ public class a6q4 {
     /**
      * @param args the command line arguments
      */
+    
+    public static double compoundInterest(double compoundInterest){
+        //return the answer of the account balance once interest is added
+        //make an int=# of years (n)
+        //make a double=interest rate (r)
+        //make a double =initial$ for interest to be added too (P)
+        //B=P(1+r)^n 
+        //answer= (B)
+        double answer = Math.pow(1,2);
+        return answer;
+    }
+        
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        //create a scanner
     }
 }
