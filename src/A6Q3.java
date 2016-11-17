e58f41327ca2c4b649f200be60597e2061cab6fe
manok5757 <manok5757@HRH-IC0027323.wrdsb.ca>
2016-11-17 10:15:37 -0500

    A5 complete

--- src/A6Q3.java
+++ src/A6Q3.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author manok5757
@@ -13,24 +12,31 @@ import java.util.Scanner;
 public class A6Q3 {
 
     // create a method to list all the factors of a number
-    public static int factors(int num){
-        int num1= 0;
-        for(int numbers = 0; numbers < num; numbers = numbers+1){
-        if(int factor % 2 == 0 ){
-            
+    public static void factors(int numEntered) {
+        // make the method only test each number for possible factors from 0 the number inputed
+        for (int numbers = 1; numbers <= numEntered; numbers = numbers + 1) {
+            // if there is no remainder, then the number is a factor
+            if (numEntered % numbers == 0) {
+                // output the factor of the number if it divides evenley
+                System.out.println("" + numbers);
+            }
+
+
+
         }
-        
-        
+
     }
-        
-    }
-    
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // create a scenner to collect input
         Scanner input = new Scanner(System.in);
+        // ask the user for the number they wish to find the factors of
+        System.out.println("Please enter a number you wish to find the factors of");
+        // store the number entered
+        int numEntered = input.nextInt();
+        factors(numEntered);
     }
 }
