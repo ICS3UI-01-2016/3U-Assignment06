d0f8535bb8cce38cef1a18e2f87b69ad4df6fb69
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2016-11-21 10:10:39 -0500

    a6q5 start

--- src/a6q5.java
+++ src/a6q5.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +15,18 @@ public class a6q5 {
     /**
      * @param args the command line arguments
      */
+    
+    public static void chaotic(int numLines){
+        //create a pattern that prints 1><5 stars on each line
+        // formula for 0 upto but not including 1: int randNum = (int)(Math.random()*(n - a + 1))+ a
+        //print to screen?
+        //cannot be zero
+    }
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create input for user
+         Scanner input = new Scanner(System.in);
+         //let user answer (let them insert "n"? and then print to screen 
     }
 }
