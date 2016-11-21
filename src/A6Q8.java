4c10bd2f40834d020b5268e199165c74ab2c7152
baayl <baayl@192.168.0.10>
2016-11-21 18:02:35 -0500

    Comments are kinda done ? :thinking:

--- src/A6Q8.java
+++ src/A6Q8.java
@@ -10,25 +10,25 @@
  */
 public class A6Q8 {
     public static void Season(int month, int day){
-        //
+        // If day/month falls in 12/16 to 3/16 it is winter
         if (day >= 16 || day <= 15) {
             if (month >= 12 || month <= 3) {
                 System.out.println("The season is winter");
             }
         }
-        //
+        // If day/month falls in 3/16 to 6/15 it is spring
         if (day >= 16 || day <= 15) {
             if (month >= 3 || month <= 6) {
                 System.out.println("The season is spring");
             }
         }
-        //
+        // If day/month falls in 6/16 to 9/15 it is summer
         if (day >= 16 || day <= 15) {
             if (month >= 6 || month <= 9) {
                 System.out.println("The season is summer");
             }
         }
-        //
+        // If day/month falls in 9/16 to 1/15 it is fall
         if (day >= 16 || day <= 15) {
             if (month >= 9 || month <= 1) {
                 System.out.println("The season is fall");
