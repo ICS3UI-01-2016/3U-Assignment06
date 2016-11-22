d45571ec48d528a2504a8afb7ad37fae0077e2b9
DeepakManoj <DeepakManoj@DeepakManoj10>
2016-11-22 17:33:48 -0500

    A6

--- src/A6Q7.java
+++ src/A6Q7.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -9,10 +12,27 @@
  */
 public class A6Q7 {
 
+public static void firstDigit(double numEntered){
+    
+    if(numEntered<=10){
+      double answer=  Math.round(numEntered/10);
+        System.out.println(""+answer);  
+    }
+    
+}
+        
+        
+        
+        
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create a scanner for input 
+        Scanner input = new Scanner(System.in);
+        // ask the user for a number they wish to find the first digit of
+        System.out.println("Please enter a number you wish to find the first digit of:");
+        // store the number
+        double numEntered = input.nextDouble();
     }
 }
