de17fa0fcd5576958ad0907f2989ecfd0c047eb5
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2016-11-30 09:54:41 -0500

    not finish A6Q10

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -1,37 +1,46 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author fabed2976
  */
 public class A6Q10 {
-    
-    public static boolean sameDashes (String firsterm , String secondterm){
-        int lengthone = firsterm.length ();
+
+    public static boolean sameDashes(String firsterm, String secondterm) {
+        int lengthone = firsterm.length();
         int lengthtwo = secondterm.length();
         int count = 0;
         char ch;
-        String changeword ="";
-        for (int i = 0; i < lengthone; i++){
-            
-       
-        
-            
+        for (int i = 0; i < lengthone; i++) {
+            if (firsterm.indexOf("-", 1) != secondterm.indexOf("-", 1)) {
+                return false;
+            }
         }
-        
-        
+        return true;
+
+
     }
-        
-        
-    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first term:");
+        String firsterm = input.nextLine();
+        System.out.println("Enter the second term:");
+        String secondterm = input.nextLine();
+        System.out.println(sameDashes(firsterm, secondterm));
+
+
+
+
+
+
     }
 }
