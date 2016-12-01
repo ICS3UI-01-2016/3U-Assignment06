4e06de1297eb9a9221507ef1877b628342d0de0b
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2016-12-01 10:16:18 -0500

    assign066

--- src/a6q8.java
+++ src/a6q8.java
@@ -12,13 +12,44 @@ public class a6q8 {
     /**
      * @param args the command line arguments
      */
+    
+    public static int season(month,day){
+        
+        int month = Math.pow(1,2);
+        return answer;
+                //fix this
+    
+    }
+    
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         //set two integers as parameters
         //int1=day int2=month
+        //month= 1-12
+        //day=1-31
+        //create if statements loop: 
         //12/16 and 3/15, you should return "Winter"
         //3/16 and 6/15, you should return "Spring"
         //6/16 and 9/15, you should return"Summer"
         //9/16 and 12/15, you should return "Fall"
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
