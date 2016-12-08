4745c0dfcfb83a0d6a19613253a1bb5a8c28fca2
woodc9217 <woodc9217@HRH-IC0027333.wrdsb.ca>
2016-12-08 09:26:30 -0500

    a6q1, a6q2, a6q6,7,8,9

--- src/a6q1.java
+++ src/a6q1.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+
+
 
 /*
  * To change this template, choose Tools | Templates
@@ -15,14 +18,25 @@ public class a6q1 {
      * @param args the command line arguments
      */
     //
-    public static double circleArea(double radius){
-        double answer = Math.pow(radius,2);
-        return answer;
-        
-    }
-
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+   public static double circleArea(double radius){
+    	//variable for pi
+    	double pi = 22/7;
+    	//circle area+return area
+    	double answer = pi*Math.pow(radius,2);
+    	return answer;
+    	
+	}
+	
+	public static void main(String[] args) {
+    	//test code
+    	//create input for the user
+    	Scanner input = new Scanner(System.in);
+    	//get user to input radius
+    	double r = input.nextDouble();
+    	double a = circleArea(r);
+	//print area
+    	
+    	System.out.println(a);
+	}
 }
+
--- src/a6q2.java
+++ src/a6q2.java
@@ -17,34 +17,44 @@ public class a6q2 {
      */
     
         
-    public static double examGrade(double examgrade){
-        double answer = Math.pow(examgrade,2);
-        return answer;
-        
-    }
-       
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //create input for the user
-        Scanner input= new Scanner(System.in);
-        System.out.println("Please type in exam grade in %");
-        //let user answer
-        int grade = input.nextInt();
-        //create conditions
-        if(grade<50){
-            System.out.println("F");
-        }else if(grade<59){
-            System.out.println("D");
-        }else if(grade<69){
-            System.out.println("C");
-        }else if(grade<79){
-            System.out.println("B");
-        }else
-            System.out.println("A");
-        
-        
-        
-        
-    }
+    public static String examGrade(double grade){
+    	//define letter
+    	String letter = "";
+    	//create conditions
+    	if(grade<50){
+        	letter = "F";
+    	}else if(grade<59){
+        	letter = "D";
+    	}else if(grade<69){
+        	letter = "C";
+    	}else if(grade<79){
+        	letter = "B";
+    	}else
+        	letter = "A";
+    	return letter;
+    	
+	}
+   	
+	
+	public static void main(String[] args) {
+    	// TODO code application logic here
+    	//create input for the user
+    	Scanner input= new Scanner(System.in);
+    	System.out.println("Please type in exam grade in %");
+    	//let user answer
+    	int grade = input.nextInt();
+    	//call method and print to test
+    	String l = examGrade(grade);
+    	
+        	System.out.println(l);
+    	
+    	
+    	
+    	
+	}
 }
+
+
+
+
+
