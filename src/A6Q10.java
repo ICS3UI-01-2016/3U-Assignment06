c02f7b67ccdbb6a03c7cf190e63563e182afa228
fabed2976 <fabed2976@HRH-IC0027295.wrdsb.ca>
2016-12-21 09:03:05 -0500

    Finished A6Q10

--- src/A6Q10.java
+++ src/A6Q10.java
@@ -12,29 +12,52 @@ import java.util.Scanner;
 public class A6Q10 {
 
     public static boolean sameDashes(String firsterm, String secondterm) {
+        //create a variable to find the length of firsterm and the secondterm
         int lengthone = firsterm.length();
         int lengthtwo = secondterm.length();
-        int count = 0;
-        char ch;
-        for (int i = 0; i < lengthone; i++) {
-            if (firsterm.charAt(i) == '-') {
-                if (secondterm.charAt(i) == '-') {
-                    return true;
-                } else {
-                   return false;
-                }
+        
+        // create a variable lengththree to store in the bigger length.
+        int lengththree = lengthone;
+
+        //Create a string called compare term one
+        String comparetermone = "";
+        //if lengthone is bigger than lengthtwo then take out the substring in firstterm at the end of length two
+        if (lengthone > lengthtwo) {
+            comparetermone = firsterm.substring(lengthtwo);
+            lengththree = lengthtwo;
         }
+        
+        // if lengthtwo is bigger than lengthone then take ou the substring in secondterm at the end of length one
+        if (lengthtwo > lengthone) {
+            comparetermone = secondterm.substring(lengthone);
+            lengththree = lengthone;
+         
         }
+        // create a foor loop using the data on lengththree
+        for (int count = 0; count < lengththree; count = count + 1) {
+            // if firsterm has a dash and secondterm does not have dash or second term has a dash and the first term does not have dash
+            if ((firsterm.charAt(count) == '-') && (secondterm.charAt(count) != '-' || secondterm.charAt(count) == '-') && (firsterm.charAt(count) != '-')) {
+                // return flase when you are outputing
+                return false;
+                
+            }
+        }
+        // if the longer statement has a dash and is equal than 0
+        if (comparetermone.indexOf("-") >= 0) {
+            // return false when outputing
+            return false;
+        }
+        // if none of the if statements are not inorder in the return false above
+        // make return true as default
+        return true;
+
     }
-//           
-            
 
-            /**
-             * @param args the command line arguments
-             */
-    
 
-    public static void main(String[] args) {
+/**
+ * @param args the command line arguments
+ */
+public static void main(String[] args) {
         //create an input for the user
         Scanner input = new Scanner(System.in);
         
@@ -46,8 +69,10 @@ public class A6Q10 {
         System.out.println("Enter the second term:");
         String secondterm = input.nextLine();
         
-        // the computer will print our the answer usign the methiod 
-        System.out.println(sameDashes(firsterm, secondterm));
+        // apply the boolean method above if it works perfectly
+       boolean sameDashes = sameDashes (firsterm, secondterm);
+       // print the answers
+       System.out.println(sameDashes);
 
 
 
