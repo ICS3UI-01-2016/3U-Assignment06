94dabc4ebea8dbb8d8a969d82a0297d4ab29aae9
Candice <Candice@192.168.0.20>
2017-01-21 17:25:06 -0500

    assign06 q9 done

--- src/a6q9.java
+++ src/a6q9.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,22 +17,49 @@ public class a6q9 {
      */
     
     
-    public static double allDigitsOdd(int even,int odd){
-        int ld = odd%10;
-        if(even<0){
-            //return "true";
+    public static boolean allDigitsOdd(int num){
+        //initialize variable for if num is all odd #s
+        boolean allodd = true;
+        
+        while(num>9){
+            //get last digit of number, then get remainder and if 0 then =even
+            if(lastDigit(num)%2==0){
+                allodd= false;
+            }
+            num = num/10;
         }
+        if(num%2==0){
+                allodd= false;
+            }    
+        return allodd;
+        
+    }
+    
+    public static int lastDigit(int num){
+        //create variable for last digit
+        int ld = num%10;
+        //if input # is negative
+        if(num<0){
+            ld= (num%10)*-1;
+        }
+        //return the last digit
         return ld;
-        //this is wrong^^fix
-        //boolean needed?
     }
     
     
     
     
+    
     public static void main(String[] args) {
-        //if any digits in the random number..
-        //even=false
-        //odd=true
+        
+        //test code
+        //create input for the user
+        Scanner input= new Scanner(System.in);
+        //let user type in #
+        int num1 = input.nextInt();
+        //call method & create variable for last digit
+        boolean allodd =allDigitsOdd(num1);
+        //print 
+        System.out.println(allodd);
     }
 }
